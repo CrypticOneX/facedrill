ca524e75dc78f2561f4ff91c4f25662e25920f62
Ashutosh Singh <37180166+CrypticOneX@users.noreply.github.com>
2025-08-14 10:11:33 +0530

    Update connect.inc.php

--- connect.inc.php
+++ connect.inc.php
@@ -1,8 +1,9 @@
 <?php
     error_reporting(0);
-    $conn=mysqli_connect('localhost','id2553909_ashutosh','Ffrcut%64gjrRmca%ffbqzx');
-    $note=mysqli_select_db($conn,'id2553909_darknight');
+    $conn=mysqli_connect('localhost','username','password');
+    $note=mysqli_select_db($conn,'db_name');
     if(!$note) {
     	 die("Error in Establishing Database Connection!");
     }
+
 ?>
